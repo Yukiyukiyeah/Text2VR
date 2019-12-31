6acaa89946395aea805772106627086d3738ed2a
yukiyukiyeah <yukitang@mail.bnu.edu.cn>
2019-12-31 00:19:59 -0800

    json

--- text2vr_util.py
+++ text2vr_util.py
@@ -1,22 +1,27 @@
 """
 @Description This file aims to extract some features from text for 3D scene building
 @Author Yuki Tang
-@Date
+@Date 2019-12-29
 """
 import sys
 import empath
 from textblob import TextBlob
 import re
 import json
+import paralleldots
+
+paralleldots.set_api_key("6opLDf4BQzxKSeGWAfcPVV241zc9Huhn31mwtFy8pYo")
 
 USERNAME = 'test'
 DATE = '20191229'
-TEXT_FILEPATH = r'./text/{username}_{date}.txt'
+FILENAME = r'{username}_{date}'.format(username = USERNAME, date = DATE)
+TEXT_FILEPATH = r'./data/{filename}.txt'
+JSON_FILEPATH = r'./data/{filename}.json'
 
 
 def get_file():
     try:
-        with open(TEXT_FILEPATH.format(username = USERNAME,date = DATE),'rt') as textfile:
+        with open(TEXT_FILEPATH.format(filename = FILENAME),'rt') as textfile:
             text = textfile.read()
             # print (type(text))
             return text
@@ -25,21 +30,42 @@ def get_file():
 
 
 def overall_analysis(text):
-    pass
+    dict_data = {}
+    
+    blob = TextBlob(text)
+    total = len(blob.sentences)
+    dict_data['size'] = total
+    
+    tags = blob.tags
+    nouns = []
+    for tag in tags:
+        if tag[1] == "NN":
+            nouns.append(tag[0])    
 
-def split_text():
-    pass
+    dict_data['noun'] = nouns
+    dict_data['noun_phrases'] = blob.noun_phrases
+    dict_data['words'] = blob.words
+    dict_data['polarity'] = round(blob.sentiment.polarity,2)
 
-def split_analysis():
-    pass
+    sentences = blob.sentences
+    print(type(sentences))
+    dict_data['sentences'] = []
+    dict_sentence = dict_data['sentences']
+    for i in range(len(sentences)):
+        dict_sentence.append({})
+        dict_sentence[i]['id'] = i
+        dict_sentence[i]['content'] = str(sentences[i])
+        dict_sentence[i]['polarity'] = round(sentences[i].sentiment.polarity,2)
 
-def split_word():
-    pass
+    print(dict_data)    
+    return dict_data
 
 def save_json():
-    pass
-
-
+    text = get_file()    
+    data = overall_analysis(text)    
+    with open(JSON_FILEPATH,"w") as f:
+        json.dump(data,f)
+        print("Saved in json file")
 
 if __name__ == "__main__":
-    get_file()
+    save_json()
